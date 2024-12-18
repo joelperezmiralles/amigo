56b9c2e8cab528a001a229910929ddac638e474a
joelperezmiralles <joelrapitecnic@gmail.com>
2024-12-18 13:04:50 +0100

    paosp

--- server.js
+++ server.js
@@ -164,7 +164,7 @@ app.get('/test', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
